0309900c7c21616bd1a2c0892471d535123bb003
ljr5656 <1368298676@qq.com>
2024-04-12 08:02:44 +0800

    init

---
+++ apps/low-code-backend/src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Version } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get()
+  @Version()
+  findAll() {
+    return this.appService.findAll();
+  }
+}
