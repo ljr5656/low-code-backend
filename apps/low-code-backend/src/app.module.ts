c615c6ff4e13dd06e11265e47da4a90468491823
ljr <1368298676@qq.com>
2024-04-15 17:49:53 +0800

    init

--- apps/low-code-backend/src/app.module.ts
+++ apps/low-code-backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { getConfig } from '../../../lib/common/src/utils';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+      isGlobal: true,
+      load: [getConfig],
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/low-code-backend/src/main.ts
+++ apps/low-code-backend/src/main.ts
@@ -1,13 +1,43 @@
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-import { VersioningType } from '@nestjs/common'
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+import { AllExceptionsFilter } from '../../../lib/common/src/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from '../../../lib/common/src/exceptions/http.exception.filter';
+import { TransformInterceptor } from '../../../lib/common/src/interceptors/transform.interceptor';
+import { generateDocument } from './doc';
 
+declare const module: any;
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  // 统一响应体格式
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  // 异常过滤器
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+
+  // 接口版本化管理
   app.enableVersioning({
-    defaultVersion: '1',
-    type: VersioningType.URI
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'],
+    type: VersioningType.URI,
   });
+
+  // 创建文档
+  generateDocument(app);
+
+  // 添加热更新
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 
   await app.listen(3000);
 }
